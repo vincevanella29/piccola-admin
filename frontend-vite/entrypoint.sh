23049355a0406f0c86686ca4827223605f84d6df
Lucciano Vanella <lucciano@Luccianos-Mac-Studio.local>
2026-03-17 05:47:10 -0300

    fix: restaurar vars completas en entrypoint.sh env.js

--- frontend-vite/entrypoint.sh
+++ frontend-vite/entrypoint.sh
@@ -7,10 +7,17 @@ mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
 # Genera /usr/share/nginx/html/env.js con todas las variables VITE_*
 cat <<EOF > /usr/share/nginx/html/env.js
 window.env = {
+  VITE_RPC_URL: "${VITE_RPC_URL}",
   VITE_API_URL: "${VITE_API_URL}",
+  VITE_BLOCK_EXPLORER: "${VITE_BLOCK_EXPLORER}",
+  VITE_PRIVY_APP_ID: "${VITE_PRIVY_APP_ID}",
   VITE_MODE: "${VITE_MODE}",
+  VITE_CHAIN_ID: "${VITE_CHAIN_ID}",
   VITE_GOOGLE_MAPS_API_KEY: "${VITE_GOOGLE_MAPS_API_KEY}",
+  VITE_COMPANY_ID: "${VITE_COMPANY_ID}",
   VITE_GOOGLE_MAP_ID: "${VITE_GOOGLE_MAP_ID}",
+  VITE_VAPID_KEY: "${VITE_VAPID_KEY}",
+  VITE_DRIP_ACCOUNT_ID: "${VITE_DRIP_ACCOUNT_ID}",
 };
 EOF
 
